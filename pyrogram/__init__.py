9a49707d224190e350dfce66961ed153b5686a7e
𝗜𝒵𝗔𝗡𝗔 <halwakk07@gmail.com>
2025-07-29 17:46:49 +0545

    Initial commit
    Created by @Endtrz 
    
    - Supports message, callback_query, inline_query, and more.
    - Integrates custom filters (dev_cmd, owner_cmd, gc_admin, etc.)
    - Automatically registers with client if available.
    - Enables clean decorator-based command routing.

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -2,7 +2,7 @@
 
 
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
 __copyright__ = "Copyright (C) 2025-Endtrz<https://github.com/Endtrz>"
 
